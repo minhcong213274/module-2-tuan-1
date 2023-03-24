96b43d2e170dc896cc8f387ae270acbd61f94b81
minhcong213274 <gathidungru@gmail.com>
2023-03-24 10:36:18 +0700

    findstudens

--- src/cong/findValue.java
+++ src/cong/findValue.java
@@ -1,2 +1,17 @@
-package cong;public class findValue {
+package cong;
+import java.util.Scanner;
+
+public class findValue {
+    public static void main(String[] args) {
+        String[] students = {"Christian", "Michael", "Camila", "Sienna", "Tanya", "Connor", "Zachariah", "Mallory", "Zoe", "Emily"};
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("input student name");
+        String name = scanner.nextLine();
+        for (int i =0;i<students.length;i++){
+            if(students[i].equalsIgnoreCase(name)) {
+                System.out.println(i);
+                return;
+            }
+        }System.out.println("cannot find studen");
+    }
 }
