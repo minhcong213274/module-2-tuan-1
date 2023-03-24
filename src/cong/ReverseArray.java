dd6f3bb92627d01b0f9768136f5962e716ddf1e4
minhcong213274 <gathidungru@gmail.com>
2023-03-24 10:19:05 +0700

    ReverseArray

--- src/cong/ReverseArray.java
+++ src/cong/ReverseArray.java
@@ -1,2 +1,23 @@
-package cong;public class ReverseArray {
+package cong;
+
+public class ReverseArray {
+    public static class Main {
+        public static void reverseArray(int[] arr) {
+            int left = 0;
+            int right = arr.length - 1;
+            while (left <= right) {
+                int temp = arr[left];
+                arr[left++] = arr[right];
+                arr[right--] = temp;
+            }
+        }
+
+        public static void main(String[] args) {
+            int[] arr = {1, 2, 3, 4, 5};
+            reverseArray(arr);
+            for (int j : arr) {
+                System.out.print(j + " ");
+            }
+        }
+    }
 }
